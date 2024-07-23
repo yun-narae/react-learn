67d3089cbf7ea5800a77e189a8ed9ec7e98e3a5b
seulbinim <skfo0827@naver.com>
2024-07-23 09:05:25 +0900

    🎁 - 마크업 - 스타일 - js템플릿 - READEME

--- 02-design-handoff/src/main.js
+++ 02-design-handoff/src/main.js
@@ -1,1 +1,44 @@
-import "./styles/main.scss";
+const data = [
+    {
+        src : "./assets/architecture01.svg",
+        alt : "건축사",
+        figcaption : "History of Architecture",
+    },
+    {
+        src : "./assets/architecture02.svg",
+        alt : "건축설계",
+        figcaption : "Building design",
+    },
+    {
+        src : "./assets/architecture03.svg",
+        alt : "기후학",
+        figcaption : "Climatology",
+    },
+    {
+        src : "./assets/architecture04.svg",
+        alt : "제도법",
+        figcaption : "Graphics",
+    },
+]
+
+function renderList() {
+    const listWrapper = document.querySelector(".list_warpper");
+  
+    for (let item of data) {
+      const template = `
+        <li>
+          <figure>
+            <img
+              src="${item.src}"
+              alt="${item.alt}"
+            />
+            <figcaption>${item.figcaption}</figcaption>
+          </figure>
+        </li>
+      `;
+  
+      listWrapper.insertAdjacentHTML('beforeend', template);
+    }
+  }
+  
+  renderList();
